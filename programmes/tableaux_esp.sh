af65b493af4ac4d6761f854daeaede99223e0b21
PatriciaAugustyn <patricia.augustyn@sorbonne-nouvelle.fr>
2023-11-29 20:20:31 +0100

    Modif des programmes des tableaux

--- programmes/tableaux_esp.sh
+++ programmes/tableaux_esp.sh
@@ -24,20 +24,7 @@ echo "<html>
 		<meta charset="UTF-8"/>
 		<title>Tableau</title>
 		<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css\">
-        <link href='https://fonts.googleapis.com/css2?family=Abel&family=Bad+Script&display=swap' rel=\"stylesheet\">
-        <style>
-            html{
-                background: #FAF0E6;
-            }
-
-            body {
-                font-family: Abel;
-
-            }
-
-            p {
-                font-size : 20px;
-            }
+		<style>
 
             table {
                 box-shadow: 2px 2px 12px rgba(0, 0, 0, 0.8);
@@ -49,30 +36,18 @@ echo "<html>
             }
 
             tr:hover {
-                color: #FBFFFF;
-                background-color: black;
+                background-color: #ace0f9;
                 transform: scale(1.01);
             }
         </style>
+	</head>
 	</head>
 	<body>"
 
 
 echo "
-			 <div class=\"content m-3\">
-				<h1 class=\"has-text-centered notification has-background-warning-light\">Résultat du tableau</h1>
-				<p>Voici le résultat final du tableaux. Pour cela vous trouverez : </p>
-				<ul>
-					<li>Le numéro de la ligne</li>
-					<li>Le lien URL de la page web</li>
-					<li>Le code HTTP, c'est-à-dire la réponse du navigateur lorsque l'utilisateur se trouve sur le site</li>
-					<li>L'encodage de la page</li>
-					<li>Le lien des pages aspirées au format HTML</li>
-					<li>Le lien des dumps-text en .txt</li>
-					<li>Le nombre d'occurrences</li>
-					<li>Le contexte droit et gauche en .txt</li>
-				</ul>
-				<table class=\"table is-bordered\">
+		<div class=\"content m-3\">
+			<table class=\"table is-bordered\">
 				<tr>
                     <th>Ligne</th>
                     <th>URL</th>
@@ -93,7 +68,7 @@ do
 	encodage=$(curl -s -I -L -w "%{content_type}" -o /dev/null $URL | grep -P -o "charset=\S+" | cut -d"=" -f2 | tail -n 1)
 	aspirations=$(curl -o "../aspirations/espagnol/esp_${lineno}.html" $URL)
 	dump=$(lynx -dump -nolist $URL > "../dumps-text/espagnol/esp_${lineno}.txt")
-	compte=$(lynx -dump "../dumps-text/espagnol/esp_${lineno}.txt" | tr '[:space:]' '\n' | egrep "felicidad" -wc)
+	compte=$(lynx -dump "../dumps-text/espagnol/esp_${lineno}.txt" | tr '[:space:]' '\n' | grep -i -o "felicidad" | wc -l)
 
 	contexte=$(grep -B 1 -A 1 -i "felicidad" "../dumps-text/espagnol/esp_${lineno}.txt")
 
